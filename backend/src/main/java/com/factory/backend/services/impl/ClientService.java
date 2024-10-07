1eb424826421e20c2167b6fcff65645321047e46
danilalisichkin <danilafox3467346@gmail.com>
2024-10-07 20:30:00 +0300

    add implementation of ClientService

--- backend/src/main/java/com/factory/backend/services/impl/ClientService.java
+++ backend/src/main/java/com/factory/backend/services/impl/ClientService.java
@@ -2,38 +2,81 @@ package com.factory.backend.services.impl;
 
 import com.factory.backend.core.dto.client.ClientAddingDTO;
 import com.factory.backend.core.dto.client.ClientDTO;
+import com.factory.backend.core.mappers.category.CategoryMapper;
+import com.factory.backend.core.mappers.client.ClientMapper;
+import com.factory.backend.entities.Category;
+import com.factory.backend.entities.Client;
+import com.factory.backend.exceptions.ResourceNotFoundException;
+import com.factory.backend.repository.CategoryRepository;
+import com.factory.backend.repository.ClientRepository;
 import com.factory.backend.services.IClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class ClientService implements IClientService {
-    @Override
-    public List<ClientDTO> getAllClients() {
-        return List.of();
+
+    private final ClientRepository clientRepository;
+
+    private final ClientMapper clientMapper;
+
+    @Autowired
+    public ClientService(ClientRepository clientRepository, ClientMapper clientMapper) {
+        this.clientRepository = clientRepository;
+        this.clientMapper = clientMapper;
     }
 
     @Override
-    public ClientDTO getClientByPhone(Long id) {
-        return null;
+    public List<ClientDTO> getAllClients() {
+        return clientRepository.findAll().stream()
+                .map(clientMapper::entityToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ClientDTO getClientByPhone(String phone) {
+        return clientMapper.entityToDto(
+                clientRepository.findById(phone).orElseThrow(
+                        () -> new ResourceNotFoundException("Client with phone=%s not found", phone)
+                )
+        );
     }
 
     @Override
     public ClientDTO saveClient(ClientAddingDTO clientDTO) {
-        return null;
+        Client client = clientRepository.save(clientMapper.addingDtoToEntity(clientDTO));
+
+        return clientMapper.entityToDto(client);
     }
 
     @Override
     public ClientDTO updateClient(ClientDTO clientDTO) {
-        return null;
+        if (clientRepository.findById(clientDTO.getPhoneNumber()).isEmpty()) {
+            throw new ResourceNotFoundException("Client with phone=%s not found", clientDTO.getPhoneNumber());
+        }
+
+        return clientMapper.entityToDto(
+                clientRepository.save(clientMapper.dtoToEntity(clientDTO))
+        );
     }
 
     @Override
     public void deleteClientByPhone(String phone) {
-
+        if (clientRepository.findById(phone).isEmpty()) {
+            throw new ResourceNotFoundException("Client with phone=%s not found", phone);
+        }
+        clientRepository.deleteById(phone);
     }
 
     @Override
     public void deleteAllClients() {
-
+        if (clientRepository.count() == 0) {
+            throw new ResourceNotFoundException("No clients found");
+        } else {
+            clientRepository.deleteAll();
+        }
     }
 }
