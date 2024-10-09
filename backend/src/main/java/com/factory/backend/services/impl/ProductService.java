62c77e5509fecfdcf1f156fa2c5e8f80c1aecfc0
danilalisichkin <danilafox3467346@gmail.com>
2024-10-09 15:48:40 +0300

    add implementation of ProductService

--- backend/src/main/java/com/factory/backend/services/impl/ProductService.java
+++ backend/src/main/java/com/factory/backend/services/impl/ProductService.java
@@ -2,38 +2,78 @@ package com.factory.backend.services.impl;
 
 import com.factory.backend.core.dto.product.ProductAddingDTO;
 import com.factory.backend.core.dto.product.ProductDTO;
+import com.factory.backend.core.mappers.product.ProductMapper;
+import com.factory.backend.entities.Product;
+import com.factory.backend.exceptions.ResourceNotFoundException;
+import com.factory.backend.repository.ProductRepository;
 import com.factory.backend.services.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class ProductService implements IProductService {
-    @Override
-    public List<ProductDTO> getAllProducts() {
-        return List.of();
+
+    private final ProductRepository productRepository;
+
+    private final ProductMapper productMapper;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
     }
 
     @Override
-    public ProductDTO getProductById(Long id) {
-        return null;
+    public List<ProductDTO> getAllProducts() {
+        return productRepository.findAll().stream()
+                .map(productMapper::entityToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ProductDTO getProductById(Integer id) {
+        return productMapper.entityToDto(
+                productRepository.findById(id).orElseThrow(
+                        () -> new ResourceNotFoundException("Product with id=%s not found", id)
+                )
+        );
     }
 
     @Override
     public ProductDTO saveProduct(ProductAddingDTO productDTO) {
-        return null;
+        Product product = productRepository.save(productMapper.addingDtoToEntity(productDTO));
+
+        return productMapper.entityToDto(product);
     }
 
     @Override
     public ProductDTO updateProduct(ProductDTO productDTO) {
-        return null;
+        if (productRepository.findById(productDTO.getId()).isEmpty()) {
+            throw new ResourceNotFoundException("Product with id=%s not found", productDTO.getId());
+        }
+
+        return productMapper.entityToDto(
+                productRepository.save(productMapper.dtoToEntity(productDTO))
+        );
     }
 
     @Override
     public void deleteProductById(Integer id) {
-
+        if (productRepository.findById(id).isEmpty()) {
+            throw new ResourceNotFoundException("Product with id=%s not found", id);
+        }
+        productRepository.deleteById(id);
     }
 
     @Override
     public void deleteAllProducts() {
-
+        if (productRepository.count() == 0) {
+            throw new ResourceNotFoundException("No products found");
+        } else {
+            productRepository.deleteAll();
+        }
     }
 }
