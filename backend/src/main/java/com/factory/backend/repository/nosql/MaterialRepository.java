eefb52a369ab7d1e264bdf44ea9a21917d0279c8
danilalisichkin <danilafox3467346@gmail.com>
2024-10-21 13:06:46 +0300

    remove deprecated mongo repository

--- backend/src/main/java/com/factory/backend/repository/nosql/MaterialRepository.java
+++
@@ -1,7 +0,0 @@
-package com.factory.backend.repository.nosql;
-
-import com.factory.backend.entities.nosql.Material;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MaterialRepository extends MongoRepository<Material, Integer> {
-}
