6c0fd60c8e46e960baa6cdf890b0475dd1ece8fe
danilalisichkin <danilafox3467346@gmail.com>
2024-10-21 13:37:49 +0300

    fix repository for MongoProduct

--- backend/src/main/java/com/factory/backend/repository/nosql/MongoProductMaterialRepository.java
+++ backend/src/main/java/com/factory/backend/repository/nosql/MongoProductMaterialRepository.java
@@ -1,7 +1,7 @@
 package com.factory.backend.repository.nosql;
 
-import com.factory.backend.entities.sql.ProductMaterial;
+import com.factory.backend.entities.nosql.MongoProductMaterial;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MongoProductMaterialRepository extends MongoRepository<ProductMaterial, String> {
+public interface MongoProductMaterialRepository extends MongoRepository<MongoProductMaterial, String> {
 }
