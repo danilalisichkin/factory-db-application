7f6b64b8ddcca8cd4efa63bb386ce878a62a6adc
danilalisichkin <danilafox3467346@gmail.com>
2024-10-23 09:27:18 +0300

    fix scripts for MongoDB

--- database/MongoDB/insert-scripts/04-insert-records.js
+++ database/MongoDB/insert-scripts/04-insert-records.js
@@ -1,35 +1,35 @@
-use factory_db;
+db = db.getSiblingDB('factory_db');
 
 db.categories.insertMany([
-    { name: "Furniture", parent_id: null },            // 1
-    { name: "Bedroom Furniture", parent_id: 1 },       // 2
-    { name: "Beds", parent_id: 2 },                    // 3
-    { name: "Nightstands", parent_id: 2 },             // 4
-    { name: "Living Room Furniture", parent_id: 1 },   // 5
-    { name: "Coffee Tables", parent_id: 5 },           // 6
-    { name: "Cabinets", parent_id: 5 },                // 7
-    { name: "Kitchen Furniture", parent_id: 1 },       // 8
-    { name: "Bar Stools", parent_id: 8 },              // 9
-    { name: "Dining Tables", parent_id: 8 }            // 10
+    { _id: NumberInt(1), name: 'Furniture', parent_id: null },            // 1
+    { _id: NumberInt(2), name: 'Bedroom Furniture', parent_id: 1 },       // 2
+    { _id: NumberInt(3), name: 'Beds', parent_id: 2 },                    // 3
+    { _id: NumberInt(4), name: 'Nightstands', parent_id: 2 },             // 4
+    { _id: NumberInt(5), name: 'Living Room Furniture', parent_id: 1 },   // 5
+    { _id: NumberInt(6), name: 'Coffee Tables', parent_id: 5 },           // 6
+    { _id: NumberInt(7), name: 'Cabinets', parent_id: 5 },                // 7
+    { _id: NumberInt(8), name: 'Kitchen Furniture', parent_id: 1 },       // 8
+    { _id: NumberInt(9), name: 'Bar Stools', parent_id: 8 },              // 9
+    { _id: NumberInt(10), name: 'Dining Tables', parent_id: 8 }           // 10
 ]);
 
 db.products.insertMany([
-    { name: "Royal Bed", category_id: 3, price: 3200.00 },                 // 1
-    { name: "Slavyanka Bed", category_id: 3, price: 2200.00 },             // 2
-    { name: "Feast Table", category_id: 10, price: 1400.00 },              // 3
-    { name: "Two Nightstands Plus Table", category_id: 4, price: 400.00 }, // 4
-    { name: "Glass Cube Cabinet", category_id: 7, price: 2000.00 }         // 5
+    { _id: NumberInt(1), name: 'Royal Bed', category_id: 3, price: 3200.00 },                 // 1
+    { _id: NumberInt(2), name: 'Slavyanka Bed', category_id: 3, price: 2200.00 },             // 2
+    { _id: NumberInt(3), name: 'Feast Table', category_id: 10, price: 1400.00 },              // 3
+    { _id: NumberInt(4), name: 'Two Nightstands Plus Table', category_id: 4, price: 400.00 }, // 4
+    { _id: NumberInt(5), name: 'Glass Cube Cabinet', category_id: 7, price: 2000.00 }         // 5
 ]);
 
 db.materials.insertMany([
-    { name: "Wood - Birch", supplier_name: 'LLC "Lesogryzy"' },               // 1
-    { name: "Wood - Oak", supplier_name: 'LLC "Lesogryzy"' },                 // 2
-    { name: "Acrylic Glass", supplier_name: 'OJSC "Glass Factory "Neman""' }, // 3
-    { name: "Fiberboard", supplier_name: 'OJSC "MaterialPlus"' },             // 4
-    { name: "Particle Board", supplier_name: 'OJSC "MaterialPlus"' },         // 5
-    { name: "Gold", supplier_name: 'OJSC "BelJewelryTrade"' },                // 6
-    { name: "Natural Leather", supplier_name: 'LLC "Wild Hunter"' },          // 7
-    { name: "Textile", supplier_name: 'LLC "Minsk Textile Factory"' }         // 8
+    { name: 'Wood - Birch', supplier_name: 'LLC "Lesogryzy"' },               // 1
+    { name: 'Wood - Oak', supplier_name: 'LLC "Lesogryzy"' },                 // 2
+    { name: 'Acrylic Glass', supplier_name: 'OJSC "Glass Factory "Neman""' }, // 3
+    { name: 'Fiberboard', supplier_name: 'OJSC "MaterialPlus"' },             // 4
+    { name: 'Particle Board', supplier_name: 'OJSC "MaterialPlus"' },         // 5
+    { name: 'Gold', supplier_name: 'OJSC "BelJewelryTrade"' },                // 6
+    { name: 'Natural Leather', supplier_name: 'LLC "Wild Hunter"' },          // 7
+    { name: 'Textile', supplier_name: 'LLC "Minsk Textile Factory"' }         // 8
 ]);
 
 db.product_materials.insertMany([
