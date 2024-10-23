44fff08084b3a8f34de3d8bb908fa8f5ff3b1855
danilalisichkin <danilafox3467346@gmail.com>
2024-10-23 10:32:06 +0300

    add MongoDB document to store ids for other documents

---
+++ backend/src/main/java/com/factory/backend/entities/nosql/Identifier.java
@@ -0,0 +1,32 @@
+package com.factory.backend.entities.nosql;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("identifiers")
+public class Identifier {
+    @Id
+    private ObjectId id;
+
+    @Indexed(unique=true)
+    private Integer categoryId;
+
+    @Indexed(unique=true)
+    private Integer clientId;
+
+    @Indexed(unique=true)
+    private Integer materialId;
+
+    @Indexed(unique=true)
+    private Integer productId;
+}
